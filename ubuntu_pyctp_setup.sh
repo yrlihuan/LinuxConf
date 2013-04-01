32c5297a4c056ee47ce443363b724f6fe4165120
Huan Li <yrlihuan@gmail.com>
2013-04-01 14:21:44 +0800

    machine setup script for pyctp machine

---
+++ ubuntu_pyctp_setup.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------
+# variables
+# ----------------------------------------------
+
+# ----------------------------------------------
+# install development tools (git, ruby, etc)
+# ----------------------------------------------
+
+sudo apt-get install -y git-core
+sudo apt-get install -y ruby1.8
+sudo apt-get install -y rubygems1.8
+sudo apt-get install -y vim
+
+# ----------------------------------------------
+# setup ssh crendentials
+# ----------------------------------------------
+
+SSH_HOME=$HOME/.ssh
+
+# add authorized keys (me622)
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsyqvf+IQr8NvUFWPutNN6PMftGEqAWJAtBbVaLszgmuLPUkItfX9OKGveaFoTISxwoAKSQ5Qy5xfDnTu23PIkoSnu6JjPcdT1qPdkLMNw3EImROLPRuJ+SpzQpvpAIbfVA1YUDlBsVnezFqNmufGi3GQdOFjciOW4N5nXWpK7dBgtEFIUlpfIiWJDwYWvrUPLnAcX1zJ6fp9t3xHU1anRM1esy9cLEMuf0qFUiA+uoHQ1Js1nbMABfl3nyYWyTIA0E9+PaOESuVFLxbbzxqXczsEe7rZ8M6EDk1ElGtLrqhFMDL1Y+OkKe779z92v0U91aFgXGHJX4D5xxUS29ZFV" >> $SSH_HOME/authorized_keys
+
+# add known hosts (github)
+echo "|1|8jvxbw0MZioRfu2cO6qWBlub6PY=|TEZV42i2eTFFczTPvcEox0oiXAo= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> $SSH_HOME/known_hosts
+
+# generate rsa key, adding to github
+echo "--------------------------------------------------------------------------------------"
+echo "The following steps needs operate manually. Continue when finished"
+echo "1. Run ssh-keygen"
+echo "2. Login your github account. Add the generated public key to auto authorized list"
+echo "3. When finished, press 'y'"
+
+FINISHED=read
+if [ $FINISHED != 'y' ]; then
+  exit
+fi
+
+# ----------------------------------------------
+# pull repos
+# ----------------------------------------------
+
+USER_GITHUB=yrlihuan
+WORKSPACE=$HOME/workspace
+
+mkdir $WORKSPACE
+git clone git@github.com:$USER_GITHUB/LinuxConf.git $HOME/LinuxConf
+git clone git@github.com:$USER_GITHUB/pyctp.git $WORKSPACE/pyctp
+git clone git@github.com:$USER_GITHUB/Hermes.git $WORKSPACE/Hermes
+
+
+# ----------------------------------------------
+# setup working env
+# ----------------------------------------------
+
+ln -s $HOME/LinuxConf/.vim $HOME/.vim
+ln -s $HOME/LinuxConf/.vimrc $HOME/.vimrc
+ln -s $HOME/LinuxConf/.screenrc $HOME/.screenrc
+ln -s $HOME/LinuxConf/.bashrc $HOME/.bashrc
+ln -s $HOME/LinuxConf/.bashrc $HOME/.profile
+ln -s $HOME/LinuxConf/.git-completion.bash $HOME/.git-completion.bash
+
