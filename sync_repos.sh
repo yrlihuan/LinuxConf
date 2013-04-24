bb7846ff8de62015b9ad665bba8ea32b463d0fef
Huan Li <yrlihuan@gmail.com>
2013-04-24 15:28:16 +0800

    script to sync repos

---
+++ sync_repos.sh
@@ -0,0 +1,9 @@
+WORKSPACE=$HOME/workspace
+
+cd $HOME/LinuxConf    && git pull
+cd $WORKSPACE/pyctp   && git checkout stocks && git pull
+cd $WORKSPACE/Hermes  && git pull
+cd $WORKSPACE/scripts && git pull
+cd $WORKSPACE/Ithaca  && git pull
+
+
