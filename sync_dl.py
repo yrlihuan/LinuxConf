741bda46014eea098b6cd1d3644401ef214bce1b
Huan Li <yrlihuan@gmail.com>
2016-12-01 10:38:58 +0800

    Save/Reload batch history on each command.

---
+++ sync_dl.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+def run(cmd):
+  print cmd
+  if os.system(cmd):
+    print 'failed!'
+    sys.exit(-1)
+
+if len(sys.argv) < 2:
+  print 'Need to specify server name, e.g., dl1'
+  sys.exit(-1)
+
+hostname = sys.argv[1]
+run('ssh -q %s exit' % hostname)
+run("rsync -avz --exclude='*batch*ckpt' --include='*.ckpt' --include='*.pkl' --exclude='*' %s:~/notebooks/* ~/notebooks/checkpoints" % hostname)
+#run("rsync -avz --include='requirements.txt' --exclude='*' ~/notebooks/* %s:~/notebooks" % hostname)
