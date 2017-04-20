8631c94807803be7dd9fc54734e9d50c00337c5f
Huan Li <huan.li@high-flyer.cn>
2017-04-20 22:27:38 +0800

    update t0 script.

--- sync_and_update_t0.py
+++ sync_and_update_t0.py
@@ -5,8 +5,9 @@ import argparse
 import datetime as dt
 import sys
 import os.path
+import git
 
-STRATEGIES_DIR = os.path.join(os.environ['HOME'], 'traders/strategies')
+TRADERS_DIR = os.path.join(os.environ['HOME'], 'traders')
 T0_ACCOUNTING_DIR = '/home/notebook-user/notebooks/t0_accounting'
 
 def main(args):
@@ -16,13 +17,13 @@ def main(args):
   ssh_keyfile = os.path.join(os.environ['HOME'], '.ssh/id_rsa_t0')
   src_file = 'rsync_t0@rsync.algo-trading.rocks:/home/rsync_t0/position_t0/%s.txt' % date_str
   accounting_file = os.path.join(T0_ACCOUNTING_DIR, 'traders', 't0', date_str + '.txt')
-  traders_file = os.path.join(STRATEGIES_DIR, 't0', date_str + '.txt')
+  traders_file = os.path.join(TRADERS_DIR, 'strategies', 't0', date_str + '.txt')
 
   cmd = "sudo rsync -avz -e 'ssh -i %s' %s %s && sudo chown notebook-user:notebook-user %s" % (ssh_keyfile, src_file, accounting_file, accounting_file)
   print cmd
   os.system(cmd)
 
-  cmd = "sudo cp %s %s && sudo chown huan:huan %s" % (accounting_file, traders_file, traders_file)
+  cmd = "sudo cp %s %s && sudo chown huan:huan %s && chmod -x %s" % (accounting_file, traders_file, traders_file, traders_file)
   print cmd
   os.system(cmd)
 
@@ -63,6 +64,28 @@ def main(args):
   out_contents = u'\n'.join(out_lines)
   with open(traders_file, 'w') as fout:
     fout.write(out_contents.encode('gb18030'))
+
+  # Push to traders repo.
+  print 'Creating commit...  ',
+  repo = git.Repo(TRADERS_DIR)
+  repo.git.add(traders_file)
+  if repo.is_dirty(index=False, working_tree=True, untracked_files=False):
+    repo.git.reset('HEAD', portfolio_relative_path)
+    print 'Index should be clean before updating [working tree not clean].'
+    sys.exit(-1)
+
+  if not repo.is_dirty(index=True, working_tree=False, untracked_files=False):
+    print 'Portfolio file is not changed.'
+    sys.exit(-1)
+
+  repo.git.commit('-m', 'Update portfolio for t0 (%s).' % (args.date))
+  print 'done!'
+
+  print 'Submitting...  ',
+  repo.git.svn('dcommit')
+  print 'done!'
+
+  repo.git.svn('rebase')
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
